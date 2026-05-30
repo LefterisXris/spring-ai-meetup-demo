f5a87b9262b466960d093a4b2f4dbc2b63257d3e
Eleftherios Chrysochoidis <lefterisxris@gmail.com>
2026-05-30 17:07:19 +0300

    chore: initial Spring Boot scaffold from Spring Initializr
    Maven, Spring Boot 3.5.14, Java 25, with Web, JPA, H2, and Lombok.

---
+++ src/test/java/gr/skg/java/meetup/spring_ai/SpringAiApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.skg.java.meetup.spring_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
